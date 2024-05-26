fd5501293d794bb5cf9e49b582aca53d97b85b2e
KiYo <kiyonori.nakashima@gmail.com>
2024-05-26 11:35:57 +0900

    show ResultDisplay and CalculatorButtons on MainActivity

--- app/src/main/java/com/example/comparecalculator/MainActivity.kt
+++ app/src/main/java/com/example/comparecalculator/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.comparecalculator.ui.component.CalculatorButtons
+import com.example.comparecalculator.ui.component.ResultDisplay
 import com.example.comparecalculator.ui.theme.CompareCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CompareCalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                App()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+    Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+        Spacer(modifier = Modifier.height(56.dp))
+        ResultDisplay()
+        Spacer(modifier = Modifier.height(12.dp))
+        CalculatorButtons()
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     CompareCalculatorTheme {
-        Greeting("Android")
+        App()
     }
 }
