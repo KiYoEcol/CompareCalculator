7e95689e9b261cf8af12acbb72fad8020d694bca
KiYo <kiyonori.nakashima@gmail.com>
2024-05-29 18:44:02 +0900

    Add MainViewModel.kt

---
+++ app/src/main/java/com/example/comparecalculator/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.comparecalculator.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MainViewModel : ViewModel() {
+    private val _textNumberDisplayPrice1 = MutableStateFlow("")
+    val textNumberDisplayPrice1: StateFlow<String> = _textNumberDisplayPrice1
+
+    fun onDigit(digit: String) {
+        val displayNumber = displayNumberPolicy(digit)
+        _textNumberDisplayPrice1.value = displayNumber
+    }
+
+    private fun displayNumberPolicy(digit: String): String {
+        var displayNumber: String
+        when {
+            digit == "." && textNumberDisplayPrice1.value == "" -> displayNumber = ""
+            textNumberDisplayPrice1.value == "" || (textNumberDisplayPrice1.value == "0" && digit != ".") -> displayNumber =
+                digit
+
+            digit == "." && textNumberDisplayPrice1.value.contains(".") -> displayNumber =
+                textNumberDisplayPrice1.value
+
+            else -> {
+                displayNumber = _textNumberDisplayPrice1.value
+                displayNumber += digit
+            }
+        }
+        return displayNumber
+    }
+
+    fun onClear() {
+        _textNumberDisplayPrice1.value = ""
+    }
+}
