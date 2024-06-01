fe3487333f429800ecc88e779af9d3b830b323fd
KiYo <kiyonori.nakashima@gmail.com>
2024-06-01 18:47:02 +0900

    change root container from Column to Box

--- app/src/main/java/com/example/comparecalculator/MainActivity.kt
+++ app/src/main/java/com/example/comparecalculator/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.comparecalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,7 +22,6 @@ class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CompareCalculatorTheme {
                 CompositionLocalProvider(androidx.lifecycle.compose.LocalLifecycleOwner provides androidx.compose.ui.platform.LocalLifecycleOwner.current) {
@@ -35,11 +33,19 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun App() {
-        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
-            Spacer(modifier = Modifier.height(56.dp))
-            ResultDisplay(viewModel = viewModel)
-            Spacer(modifier = Modifier.height(12.dp))
-            CalculatorButtons(viewModel = viewModel)
+        Box(Modifier.fillMaxSize()) {
+            ResultDisplay(
+                modifier = Modifier
+                    .padding(top = 56.dp, start = 8.dp, end = 8.dp)
+                    .align(Alignment.TopStart),
+                viewModel = viewModel
+            )
+            CalculatorButtons(
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(Alignment.BottomStart),
+                viewModel = viewModel
+            )
         }
     }
 
